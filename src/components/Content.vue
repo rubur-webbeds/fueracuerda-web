3e7220d457c1afdeaf94a805d1dcb8d22ed44972
tommycortes <allmy42@gmail.com>
2021-03-23 19:48:23 +0100

    add searchbar

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,15 +1,18 @@
 <template>
   <v-container>
+    <SearchBar />
     <SectoresList />
   </v-container>
 </template>
 
 <script>
 import SectoresList from './SectoresList';
+import SearchBar from './SearchBar';
 export default {
   name: "Content",
   components:{
       SectoresList,
+      SearchBar,
   },
   data: () => ({
     //
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" md="3">
+        <v-text-field label="Outlined" outlined></v-text-field>
+      </v-col>
+      <v-col cols="12" md="1">
+        <v-btn icon color="pink">
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+
+  data: () => ({
+    //
+  }),
+};
+</script>
