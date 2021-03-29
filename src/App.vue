aa28a785125e3f357d1bb6c302770dbd0238c1fd
Ruben Bustos <ruben_b_r_@hotmail.com>
2021-03-29 22:22:19 +0200

    add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <v-app>
-    <Header />
-    <v-main>
-      <Content />
-    </v-main>
-    <Footer />
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-import Header from "./components/Header";
-import Content from "./components/Content";
-import Footer from "./components/Footer";
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: "App",
-  components: {
-    Header,
-    Content, 
-    Footer,
-  },
+#nav {
+  padding: 30px;
 
-  data: () => ({
-    //
-  }),
-};
-</script>
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
