c7f03b3be7ee8702096e09f4c2f402ef00f488e2
APujol97 <pujolvillegas@gmail.com>
2021-05-09 21:56:21 +0200

    vuelayers installed + map developed

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
+import VueLayers from 'vuelayers'
+import 'vuelayers/lib/style.css'
 
 Vue.config.productionTip = false
 
@@ -10,3 +12,7 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+Vue.use(VueLayers, {
+  dataProjection: 'EPSG:4326',
+})
