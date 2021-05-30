3459f9339ca302a725ccd14d4680c87f110292d9
Ruben Bustos <ruben_b_r_@hotmail.com>
2021-05-30 19:46:47 +0200

    Json-ld test

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,6 +11,7 @@
         {{ new Date().getFullYear() }} — <strong>Fueracuerda</strong>
       </v-card-text>
     </v-card>
+    <script v-html="jsonld", type="application/ld+json">
   </v-footer>
 </template>
 
@@ -18,7 +19,21 @@
 export default {
   name: "Footer",
   data: () => ({
-    //
+    jsonld: {
+      "@context": "https://schema.org",
+      "@type": "BreadcrumbList",
+      "itemListElement": [{
+        "@type": "ListItem",
+        "position": 1,
+        "name": "Books",
+        "item": "https://example.com/books"
+      }, {
+        "@type": "ListItem",
+        "position": 2,
+        "name": "The Lord of the Rings",
+        "item": "https://example.com/books/the-lord-of-the-rings"
+      }]
+    }
   }),
 };
 </script>
