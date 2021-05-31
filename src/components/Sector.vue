32c499646c2f4c482b3ff76b8b058f15ae562302
Ruben Bustos <ruben_b_r_@hotmail.com>
2021-05-31 23:19:33 +0200

    jsonld test

--- src/components/Sector.vue
+++ src/components/Sector.vue
@@ -295,15 +295,17 @@ export default {
       return this.calas_cercanas;
     },
   },
-  // jsonld: {
-  //     "@context": "http://schema.org/",
-  //     "@type": "Mountain",
-  //     "name": this.sector.nom,
-  //     "description": this.sector.descripcio,
-  //     "photo": this.sector.imatges[0],
-  //     "latitude": this.sector.geoposicionament1.lat,
-  //     "longitude": this.sector.geoposicionament1.lon
-  //   },
+  jsonld() {
+    return {
+      "@context": "http://schema.org/",
+      "@type": "Mountain",
+      "name": this.sector.nom,
+      "description": this.sector.descripcio,
+      "photo": this.sector.imatges[0],
+      "latitude": this.sector.geoposicionament1.lat,
+      "longitude": this.sector.geoposicionament1.lon
+    }
+  },
 };
 </script>
 
